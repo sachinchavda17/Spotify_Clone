3a8963a220040b9d1e7fec0ca069a755bcecb50f
sachinchavda17 <sachinchavda321@gmail.com>
2024-01-24 23:17:42 +0530

    Update config.js

--- spotify-frontend/src/utils/config.js
+++ spotify-frontend/src/utils/config.js
@@ -1,4 +1,3 @@
-// export const backendUrl = process.env.REACT_APP_BACKEND_URL
-export const backendUrl = "http://localhost:8080"
+export const backendUrl = process.env.REACT_APP_BACKEND_URL
 export const cloudinary_cloudName = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME
 export const cloudinary_upload_preset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
